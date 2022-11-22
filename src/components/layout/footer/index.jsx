f41793c33944f6b8928c5018d66f21fd998c5abd
Aham Kingsley <kingsleyaham6@gmail.com>
2022-11-22 05:31:13 +0100

    adjusted footer

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -3,7 +3,7 @@ import FooterNav from "./components/FooterNav";
 import SocialLinks from "./components/SocialLinks";
 const Footer = () => {
   return (
-    <footer className="p-3 shadow md:px-6 md:py-6 w-full">
+    <footer className="p-3 shadow md:px-6 md:py-4 w-full">
       <hr className="my-2 border-gray-600 sm:mx-auto  lg:my-3" />
       <div className="md:flex md:items-center md:justify-between pt-1 text-center">
         <FooterLogo />
