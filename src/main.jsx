5e93fe45b65d9df3443fca3b03ff0e4757857cfd
Francis <francisalexander000@gmail.com>
2023-01-31 18:57:38 +0100

    removed react-seo import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import ReactSEO from "react-seo";
 import "react-toastify/dist/ReactToastify.css";
 // import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "./index.css";
