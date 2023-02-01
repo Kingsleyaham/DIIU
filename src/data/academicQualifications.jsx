f50062dc164a0e651b723849ed45914f479bda65
Aham Kingsley <kingsleyaham6@gmail.com>
2023-02-01 16:55:05 +0100

    edited about page

--- src/data/academicQualifications.jsx
+++ src/data/academicQualifications.jsx
@@ -5,20 +5,6 @@ const academicQualifications = {
         "Nnamdi Azikiwe University, Awka.  (Doctor of Philosophy, June, 2018)",
       award: "PhD Data Communication And Networks/ Info Tech",
     },
-    {
-      institution:
-        "Nnamdi Azikiwe University, Awka. (Master Degree, August, 2006)",
-      award: "MSc Computer Science",
-    },
-    {
-      institution:
-        "Nnamdi Azikiwe University, Awka. (Bachelor Degree, August, 1999)",
-      award: "BSc",
-    },
-    {
-      institution: "ORACLE ACADEMY (November, 2015)",
-      award: "Oracle DB Administrator",
-    },
   ],
 };
 
